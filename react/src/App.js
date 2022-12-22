fa54d5a2b5955801a8cd8614814132318287cbb7
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2022-12-21 22:46:20 -0300

    Atividade 02 React

--- react/src/App.js
+++ react/src/App.js
@@ -1,141 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Carousel from "react-bootstrap/Carousel";
-import Table from "react-bootstrap/Table";
-
 import React from "react";
-
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
-      <Navbar bg="primary" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <div style={{ background: "#212529" }}>
+      <div
+        style={{
+          paddingTop: 50,
+          paddingBottom: 50,
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          justifyContent: "space-evenly",
+          flexDirection: "row",
+        }}
+      >
+        <img
+          style={{ width: "50%", borderRadius: 7 }}
+          className="imgagem"
+          src="https://wallpapercave.com/wp/wp4923981.jpg"
+          alt="First slide"
+        />
 
-
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Carousel style={{ width: '50%' }}>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://wallpapercave.com/wp/wp4923981.jpg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://wallpaperaccess.com/full/1338370.jpg"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://wallpaperaccess.com/full/3909225.jpg"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-
-        <Container style={{ width: 300, paddingTop: 40 }}>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" placeholder="" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Salvar{" "}
-            </Button>
-          </Form>
-        </Container>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <input
+            style={{ margin: 20, padding: 10, borderRadius: 7, border: "thin" }}
+            placeholder="Login"
+          ></input>
+          <input
+            style={{ margin: 20, padding: 10, borderRadius: 7, border: "thin" }}
+            placeholder="Senha"
+          ></input>
+        </div>
       </div>
-
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
-
-      <br />
-      <br />
-    </>
+    </div>
   );
 }
 
